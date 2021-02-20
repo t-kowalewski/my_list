7a2ff4078fc42407c511394d55af0d8525310a62
t-kowalewski <wkowalewski@outlook.com>
2021-02-20 23:29:16 +0100

    add listener for backdrop & cancel btn

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,8 @@
 const addBtn = document.querySelector('header').lastElementChild;
 const backdrop = document.getElementById('backdrop');
 const addModal = document.getElementById('add-modal');
+const cancelModalBtn = document.querySelector('.modal__actions')
+  .firstElementChild;
 
 // Functions
 const addModalToggle = () => {
@@ -14,6 +16,16 @@ const backdropToggle = () => {
 
 // Event Listeners
 addBtn.addEventListener('click', () => {
+  addModalToggle();
+  backdropToggle();
+});
+
+cancelModalBtn.addEventListener('click', () => {
+  addModalToggle();
+  backdropToggle();
+});
+
+backdrop.addEventListener('click', () => {
   addModalToggle();
   backdropToggle();
 });
