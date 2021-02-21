fc1f368bd263024f6434bdeff8baaf38ca993b90
t-kowalewski <wkowalewski@outlook.com>
2021-02-21 22:29:44 +0100

    add itemsList and clearInput

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,18 @@
-// DOM Elements
+// DOM
+// Main Elements
 const addBtn = document.querySelector('header').lastElementChild;
 const backdrop = document.getElementById('backdrop');
+
+// Modals
 const addModal = document.getElementById('add-modal');
-const cancelModalBtn = document.querySelector('.modal__actions')
-  .firstElementChild;
+const modalActions = document.querySelectorAll('.modal__actions');
+const cancelModalBtn = modalActions[0].firstElementChild;
+const addModalBtn = modalActions[0].lastElementChild;
+const userInputs = addModal.querySelectorAll('input');
+////////////////////////////
+
+// DATA
+const itemsList = [];
 
 // Functions - Handlers
 const addModalToggle = () => {
@@ -19,9 +28,47 @@ const toggleAddModalAndBackdrop = () => {
   backdropToggle();
 };
 
+const clearInput = () => {
+  for (const input of userInputs) {
+    input.value = '';
+  }
+};
+
+const addItem = () => {
+  const itemTitle = userInputs[0].value;
+  const itemImg = userInputs[1].value;
+  const itemRating = userInputs[2].value;
+
+  if (
+    itemTitle.trim() === '' ||
+    itemImg.trim() === '' ||
+    itemRating.trim() === '' ||
+    +itemRating < 1 ||
+    +itemRating > 5
+  ) {
+    alert('Please enter valid value (rating between 1 and 5)');
+    return;
+  }
+
+  const newItem = {
+    title: itemTitle.trim(),
+    img: itemImg.trim(),
+    rating: itemRating.trim(),
+  };
+
+  itemsList.push(newItem);
+  console.log(itemsList);
+
+  toggleAddModalAndBackdrop();
+  clearInput();
+};
+
 // Event Listeners
 addBtn.addEventListener('click', toggleAddModalAndBackdrop);
-
-cancelModalBtn.addEventListener('click', toggleAddModalAndBackdrop);
-
 backdrop.addEventListener('click', toggleAddModalAndBackdrop);
+
+cancelModalBtn.addEventListener('click', () => {
+  toggleAddModalAndBackdrop();
+  clearInput();
+});
+addModalBtn.addEventListener('click', addItem);
